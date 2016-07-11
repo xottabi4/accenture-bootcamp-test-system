10381c9078825d10733d900536ef9d6b9ad52925
sklandrausis <janis.akmenskalns@gmail.com>
2016-07-11 14:21:22 +0100

    .

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -10,43 +10,39 @@ myapp.controller('LoginController',
     AuthSharedService.logout();
 }).controller('HomeController', function($scope, Session) {
     $scope.email = Session.email;
-}).controller("TehnicalController", function($scope, $routeParams) {
-    $scope.param = $routeParams.param;
-    $scope.message = "This is tehnical test";
-}).controller("EngilshController", function($scope, $routeParams) {
-<<<<<<< HEAD
-	$scope.param = $routeParams.param;
-	$scope.message = "This is english test";
+
 	
-}).controller ("Engilsh_test_Controlle", function ($scope ,$http){
+}).controller ("Engilsh_test_Controlle", function ($scope, $sessionStorage, $window, $location, $interval, $http){
 	
 	$http.get('http://localhost:8080/applicant/get-questions',{params: {testType: 'Language'}}).success(function(data) {
 
 		$scope.questions = data.questions;
 		$scope.option=$scope.questions.option;
-		console.log($scope.questions.option );
-		
+
+	
 	
 	})
-	
-	
-}).controller("Tehnica_test_lController", function ($scope ,$http){
+        
+    
+    
+}).controller("Tehnica_test_lController", function ($scope, $sessionStorage, $window, $location, $interval, $http){
 	
 	$http.get('http://localhost:8080/applicant/get-questions',{params: {testType: 'Technical'}}).success(function(data) {
 
 		$scope.questions = data.questions;
 		$scope.option=$scope.questions.option;
-		//console.log($scope.questions.option);
+		console.log($scope.questions.option);
 		
 	
+    
 	})
-	
-	
+			
 }).controller('getAnswers',function ($scope ,$http){
 	
 	
 	$("#getAnswers").click(function() {
 		var Answers={};
+		var answers=[];
 		$("textarea").each(function( index ){
 			
 			//console.log($( this ).val() );
@@ -75,90 +71,42 @@ myapp.controller('LoginController',
 						}
 						console.log(checkID);
 						
-						var answers=[];
+						
 						answers[index]={"id":answerID ,"Answertext": answertext, "optionChecked":optionChecked};
 						
 						Answers={"answers":answers};
-						console.log(Answers);
+						console.log(Answers);					
 								
-			})
+		/*	})
 			$http({
-                url: "http://localhost:8080/applicant/-Answers",
+                url: 'http://localhost:8080/applicant/save-answers',
                 method: 'POST',
-                params: {testType: 'Technical'},
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
-                    /*'Accept': 'text/json'*/}
-            })//.success().error();
-
+                    }
+				data: "Answers"
+            });
 			
+		*/							
+					
+			   })
+			})
+						
+			var config = {
+	                headers : {
+	                    'Content-Type': 'application/x-www-form-urlencoded'
+	                }
+			}
 			
+			 $http.post("http://localhost:8080/applicant/save-answers",{params: {testType: 'Answers'}} ,config)
+	            .success(function (Answers, status, headers, config) {
+	            	 $scope.PostDataResponse = Answers;
+	            	
+	            })
 			
-		})
-		
-		
-			
-	})
-		
-	
-	
-	
-
-//	var result= 
-		//questionsService.getquestions("Language");
-	//$scope.Languagequestions=Test.questions;
-	//console.log("       "+Test.questions);
-//	$scope.Technicalquestions=questionsService.myFunc("Technical");
-//    console.log($scope.Languagequestions);
-//    console.log($scope.Technicalquestions);
-
-    
-    
-    
-    
-=======
-    $scope.param = $routeParams.param;
-    $scope.message = "This is english test";
-
-}).controller("Engilsh_test_Controlle", function($scope, $http) {
-
-    $http.get('http://localhost:8080/applicant/get-questions', {
-        params: {
-            testType: 'Language'
-        }
-    }).success(function(data) {
-
-        $scope.questions = data.questions;
-        $scope.option = $scope.questions.option;
-        console.log($scope.questions.option);
-    })
+	})	       
 
 
-}).controller("Tehnica_test_lController", function($scope, $http) {
-
-    $http.get('http://localhost:8080/applicant/get-questions', {
-            params: {
-                testType: 'Technical'
-            }
-        }).success(function(data) {
-
-            $scope.questions = data.questions;
-            $scope.option = $scope.questions.option;
-            console.log($scope.questions.option);
-
-
-        })
-        //	var result=
-        //questionsService.getquestions("Language");
-        //$scope.Languagequestions=Test.questions;
-        //console.log("       "+Test.questions);
-        //	$scope.Technicalquestions=questionsService.myFunc("Technical");
-        //    console.log($scope.Languagequestions);
-        //    console.log($scope.Technicalquestions);
-
-
-
->>>>>>> 80b9701524f54f2a592802abc0b5e5b09e68ea4a
 }).controller('ErrorController', function($scope, $routeParams) {
     $scope.code = $routeParams.code;
     switch ($scope.code) {
@@ -172,62 +120,7 @@ myapp.controller('LoginController',
             $scope.code = 500;
             $scope.message = "Oops! unexpected error"
     }
-}).controller('TimerControllerLanguageTest', function($scope, $sessionStorage, $window, $location, $interval, $http) {
-    $http.get('http://localhost:8080/applicant/get-questions', {
-        params: {
-            testType: 'Language'
-        }
-    }).success(function(data) {
-        $scope.testDurationLanguage = data.duration;
-        if (sessionStorage.getItem("autosave") != null) { //
-            $scope.dateMilisec = sessionStorage.getItem("autosave");
-            $scope.newDate = new Date(+sessionStorage.getItem("autosave"));
-        } else {
-
-            $scope.myDate = new Date();
-            $scope.newDate = $scope.myDate.getTime() + $scope.testDurationLanguage;
-            sessionStorage.setItem("autosave", $scope.newDate);
-        }
-    })
-
-
-    $interval(callAtInterval, 5000);
-
-    function callAtInterval() {
-        console.log("Interval occurred");
-        if (sessionStorage.getItem("autosave") < Date.now()) {
-            $window.location.href = 'http://localhost:8080/#/applicant';
-        }
-    }
-
-}).controller('TimerControllerTechnicalTest', function($scope, $sessionStorage, $window, $location, $interval, $http) {
-
-    $http.get('http://localhost:8080/applicant/get-questions', {
-        params: {
-            testType: 'Technical'
-        }
-    }).success(function(data) {
-        $scope.testDurationTechnical = data.duration;
-        if (sessionStorage.getItem("autosave") != null) { //
-            $scope.dateMilisec = sessionStorage.getItem("autosave");
-            $scope.newDate = new Date(+sessionStorage.getItem("autosave"))
-
-        } else {
-            $scope.myDate = new Date();
-            $scope.newDate = $scope.myDate.getTime() + $scope.testDurationTechnical;
-            sessionStorage.setItem("autosave", $scope.newDate);
-        }
-    })
-
-
-    $interval(callAtInterval, 5000);
-
-    function callAtInterval() {
-        console.log("Interval occurred");
-        if (sessionStorage.getItem("autosave") < Date.now()) {
-            $window.location.href = 'http://localhost:8080/#/applicant';
-        }
-    }
+    
 
 
 });
